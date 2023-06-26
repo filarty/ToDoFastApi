a98d55742bb4db0b2fc1c5bd603a3ffc17ad2b8b
Artjom <filarty@gmail.com>
2023-06-26 18:57:34 +0200

    update schemas.py and tests:

--- schemas.py
+++ schemas.py
@@ -1,12 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime
 
 
 class TaskBase(BaseModel):
+    id: int
     title: str
     description: str
 
-
+    @validator('title')
+    def check_not_empty(cls, value):
+        if value == " " or value == "":
+            raise ValueError("title musst not empty!")
+        return value
 
 class TaskCreate(TaskBase):
     created_at: datetime
--- tests/schemas_test.py
+++ tests/schemas_test.py
@@ -2,26 +2,39 @@ from schemas import TaskBase, TaskCreate, TaskUpdate
 
 from datetime import datetime
 
-
 def test_task_base():
     #create test for TaskBase object
-    task = TaskBase(title="hello", description="hello")
+    task = TaskBase(id=0, title="hello", description="hello")
     assert task.title == "hello"
     assert task.description == "hello"
-    
+    assert task.id == 0
 
 def test_task_create():
     #create test for TaskCreate object
-    task = TaskCreate(title="first", description="hello", created_at=datetime.now())
+    task = TaskCreate(id=0, title="first", description="hello", created_at=datetime.now())
     assert task.title == "first"
     assert task.description == "hello"
     assert isinstance(task.created_at, datetime)
-
+    assert task.id == 0
 
 def test_task_update():
     #create test for TaskUpdate object
-    task = TaskUpdate(title="update", description="hello", update_at=datetime.now(), complated=False)
+    task = TaskUpdate(id=0, title="update", description="hello", update_at=datetime.now(), complated=False)
+    assert task.id == 0
     assert task.title == "update"
     assert task.description == "hello"
     assert isinstance(task.update_at, datetime)
     assert task.complated == False
+    
+def test_task_title_not_empty():
+    #check value for title in object Task
+    try:
+        task = TaskBase(id=0, title="", description="hello")
+    except ValueError:
+        assert True
+    
+    try:
+        task = TaskBase(id=0, title=" ", description="hello")
+    except ValueError:
+        assert True
+    
