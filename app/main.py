b1545388a19b7db5e71aecdb3f638f0c224a657b
Artjom <filarty@gmail.com>
2023-06-29 18:39:12 +0200

    modify main.py

--- app/main.py
+++ app/main.py
@@ -5,21 +5,32 @@ import uvicorn
 
 app = FastAPI()
 
-from schemas import TaskUpdate, TaskCreate
+from .schemas import TaskUpdate, TaskCreate
 
-from models import Task
-
+from database import crud
 
 @app.put("/updateTask")
 async def update(task: TaskUpdate):
+    try:
+        crud.update_task(task)
+    except:
+        return {"message": "bad request!"}
     return {"message": "Task updated!"}
 
 @app.get("/getTask")
 async def get_task(id: int):
-    return {"message": "Ok!"}
+    try:
+        task = crud.get_task(id)
+    except:
+        return {"message": "bad request!"}
+    return {"task": task}
 
 @app.post("/createTask")
 async def create_task(task: TaskCreate):
+    try:
+        crud.create_task(task)
+    except:
+        return {"message": "bad request!"}
     return {"message": "Task created!"}
 
 
